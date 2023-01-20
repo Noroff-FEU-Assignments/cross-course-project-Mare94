1b273fe64766dce241dba7c5aea36f4ca85e92ee
Mare94 <Morten.Sandstedt@stud.noroff.no>
2023-01-20 18:32:37 +0100

    img size and js changes and hmtl

--- js/game.js
+++ js/game.js
@@ -1,4 +1,5 @@
 const gamesContainer = document.querySelector(".games");
+const usedGamesContainer = document.querySelector(".used-games");
 const url = "https://powerofflower.online/cms-ma1/flower-power/wordpress/wp-json/wc/store/products";
 const corsEnabledUrl = "https://noroffcors.onrender.com/" + url;
 
@@ -25,7 +26,7 @@ async function getGameData(){
 
             gamesContainer.innerHTML += `<a href="details.html?name=${data[i].name}" class='result'>
                                             <h3>Game: ${data[i].name}</h3>
-                                            <div> <img src="${data[i].images[0].src}" /></div>
+                                            <div> <img src="${data[i].images[0].src}" class="game-img" /> </div>
                                             <h4>${data[i].short_description}</h4>
                                             <h4>${data[i].price_html}</h4></a>
                                             
@@ -41,3 +42,35 @@ async function getGameData(){
 };
 
 getGameData();
+
+async function getUsedGameData(){
+    try{
+        const response = await fetch(url);
+        const data = await response.json();
+
+        console.log(data);
+        
+        for(let i = 0; i < 8; i++){
+
+            console.log(data[i].name);
+            console.log(data[i].images.src);
+            console.log(data[i].prices.price);
+
+            usedGamesContainer.innerHTML += `<a href="details.html?name=${data[i].name}" class='result'>
+                                            <h3>Game: ${data[i].name}</h3>
+                                            <div> <img src="${data[i].images[0].src}" class="game-img" /> </div>
+                                            <h4>${data[i].short_description}</h4>
+                                            <h4>${data[i].price_html}</h4></a>
+                                            
+                                            `;
+
+
+        }
+
+    }
+    catch(error){
+
+    }
+};
+
+getUsedGameData();
