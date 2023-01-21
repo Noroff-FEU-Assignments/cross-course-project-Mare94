07d501915ce0edc5d51d10c027f966e12d44157f
Mare94 <Morten.Sandstedt@stud.noroff.no>
2023-01-21 14:40:44 +0100

    added async function

--- js/details.js
+++ js/details.js
@@ -7,3 +7,18 @@ const urlName = params.get("name");
 const url = "https://powerofflower.online/cms-ma1/flower-power/wordpress/wp-json/wc/store/products";
 
 const detailsUrl = url + urlName;
+
+async function getGameDetail(){
+   
+    try{
+
+        const response = await fetch(detailsUrl);
+        const details = await response.json();
+
+        console.log(details)
+
+    }
+    catch(error){
+        console.log("An error occured");
+    }
+}
